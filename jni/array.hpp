af957bdeecf073679c11f4cee43cb37f3a3a0d2a
stolou <rincewind25@hotmail.com>
2013-05-23 00:38:19 +0300

    - Some minor modifications in array.hpp

--- jni/array.hpp
+++ jni/array.hpp
@@ -7,19 +7,21 @@
 
 template<class T> class C_Array {
 public:
-	T* data;
 	size_t length;
 	size_t count;
 	size_t element;
 
 	C_Array();
 	C_Array(int s);
-	void push_back(T e);
+	void push_back(const T & val);
 	T pop_back();
 	T& operator[](int index);
 	void compactify();
 	unsigned int size();
 	void erase(int index);
+
+private:
+	T* data;
 };
 
 template <class T>
@@ -31,34 +33,30 @@ C_Array<T>::C_Array()
 
 template <class T>
 C_Array<T>::C_Array(int s)
-: count(0), length(s), element(sizeof(T))
+: count(0), element(sizeof(T))
 {
-	data = new T[s];
+	length = s ? s : 4;
+	data = new T[length];
+	assert(data);
 }
 
 template <class T>
-void C_Array<T>::push_back(T e)
+void C_Array<T>::push_back(const T & val)
 {
 	assert(element);
+	assert(length);
 
-	if(!length) {
-		data = new T[2];
-	}
-
+	/// Vector is full. Double the space.
 	if(count == length) {
-		/// Double the space
-		length *= 2;
-
-		T *new_data = new T[length];
-		if(!new_data) {
-			assert(0);
-		}
+		T *new_data = new T[2 * length];
+		assert(new_data);
 		memcpy((void *)new_data, (void *)data, length * element);
+		length *= 2;
 		delete[] data;
 		data = new_data;
 	}
 
-	data[count++] = e;
+	data[count++] = val;
 }
 
 template <class T>
@@ -87,19 +85,18 @@ template <class T>
 void C_Array<T>::compactify()
 {
 	assert(element);
+	assert(length);
+	assert(data);
 	assert(count <= length);
 
-	if(!data || length == count) {
+	if(length == count) {
 		return;
 	}
 
-	assert(count < length);
-
-	length = count;
+	/// Dont let it be zero
+	length = count ? count : 4;
 	T *new_data = new T[length];
-	if(!new_data) {
-		assert(0);
-	}
+	assert(new_data);
 	memcpy((void *)new_data, (void *)data, length * element);
 	delete[] data;
 	data = new_data;
@@ -115,6 +112,7 @@ template <class T>
 void C_Array<T>::erase(int index)
 {
 	assert(element);
+	assert(data);
 
 	if(!count || index < 0 || index >= count) {
 		return;
@@ -135,8 +133,8 @@ void C_Array<T>::erase(int index)
 
 	/// Else we have to do the memmove...
 	/// Find the smallest memory transfer
-	if(index < count >> 1) {
-		memmove(data + 1, data, (index - 1) * element);
+	if(index < count / 2) {
+		memmove(data + 1, data, index * element);
 		data += 1;
 	} else {
 		memmove(&data[index], &data[index + 1], (count - index - 1) * element);
