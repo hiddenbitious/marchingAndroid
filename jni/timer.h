c8060a10a91f06115a0e698a1a9481715b8b9b15
stolou <rincewind25@hotmail.com>
2013-05-26 15:49:39 +0300

    - Added some comments int timer class

--- jni/timer.h
+++ jni/timer.h
@@ -14,6 +14,21 @@
 *     ***************************       *
 ****************************************/
 
+/**
+ * Timer class.
+ * A tipical use scenario would look like this:
+ * C_Timer timer;
+ * timer.Initialize();
+ *
+ * timer.Update();
+ * float time = timer.GetDelta();
+ *
+ * // Do stuff...
+ *
+ * timer.Update();
+ * float time2 = timer.GetDelta() / 1000.0f;
+ */
+
 #ifndef _C_TIMER_H_
 #define _C_TIMER_H_
 
@@ -23,18 +38,18 @@
 
 class C_Timer {
 private:
-	float timeFactor;				//To convert the time received
-	int64_t freq;					//Timer frequency
+	float timeFactor;				/// To convert the time received
+	int64_t freq;					/// Timer frequency
 
-	float delta, d0, d1;			//Used to calculate delta
-	timeval mmTimeStart;			//Multimedia timer start value
+	float delta, d0, d1;			/// Used to calculate delta
+	timeval mmTimeStart;			/// Multimedia timer start value
 
 public:
-	C_Timer(void);					//CTor
-	void Initialize(void);			//Initializes timer
-	float GetTime(void);			//Returns since the timer initialization
+	C_Timer(void);					/// CTor
+	void Initialize(void);			/// Initializes timer
+	float GetTime(void);			/// Returns time elapsed since the initialization
 
-	void Update(void);				//Updates the delta variable
+	void Update(void);				/// Updates the delta variable
 
 	/// Returns the delta variable
 	inline float GetDelta(void) { return delta; }
